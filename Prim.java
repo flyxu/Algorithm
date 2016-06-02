ca056786f2c253fe52651a20b06c0f6a3e4ff52c
xufly <523230758@qq.com>
2016-06-02 21:03:06 +0800

    Prim

--- Prim.java
+++ Prim.java
@@ -1,95 +1,107 @@
-package xulfy;
+package Algorithm;
 
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+/*
+ * 最小生成树
+ * Prim algorithm
+ */
+//顶点
+class vertex{
+	String key;
+	vertex(String key){
+		this.key=key;
+	}
+}
+
+//边
+class edge{
+	vertex start;
+	vertex end;
+	int key;
+	edge(vertex start,vertex end,int key){
+		this.start=start;
+		this.end=end;
+		this.key=key;
+	}
+}
 
 public class Prim {
-
-	public static List<Vertex> vertexList = new ArrayList<Vertex>();//结点集
-	public static List<Edge> EdgeQueue = new ArrayList<Edge>();//边集
-	public static List<Vertex> newVertex = new ArrayList<Vertex>();//已经 访问过的结点
-	
-	public static void main(String[] args) {
-		primTree();
-
+	public static List<vertex> vertexlist=new ArrayList<vertex>();//顶点集
+	public static List<edge> edgelist=new ArrayList<edge>();//边集
+	public static List<vertex> newVertex=new ArrayList<vertex>();//已访问过的顶点集合
+	public static void buildgraph(){
+		vertex v1=new vertex("a");
+		vertex v2=new vertex("b");
+		vertex v3=new vertex("c");
+		vertex v4=new vertex("d");
+		vertex v5=new vertex("e");
+		vertexlist.add(v1);
+		vertexlist.add(v2);
+		vertexlist.add(v3);
+		vertexlist.add(v4);
+		vertexlist.add(v5);
+		addedge(v1, v2, 6);
+		addedge(v1, v3, 7);
+		addedge(v2, v3, 8);
+		addedge(v2, v5, 4);
+		addedge(v2, v4, 5);
+		addedge(v3, v4, 3);
+		addedge(v3, v5, 9);
+		addedge(v4, v5, 7);
+		addedge(v5, v1, 2);
+		addedge(v2, v4, 2);
 	}
-	public static void buildGraph() {
-		Vertex v1 = new Vertex("a");
-		Prim.vertexList.add(v1);
-		Vertex v2 = new Vertex("b");
-		Prim.vertexList.add(v2);
-		Vertex v3 = new Vertex("c");
-		Prim.vertexList.add(v3);
-		Vertex v4 = new Vertex("d");
-		Prim.vertexList.add(v4);
-		Vertex v5 = new Vertex("e");
-		Prim.vertexList.add(v5);
-		addEdge(v1, v2, 6);
-		addEdge(v1, v3, 7);
-		addEdge(v2, v3, 8);
-		addEdge(v2, v5, 4);
-		addEdge(v2, v4, 5);
-		addEdge(v3, v4, 3);
-		addEdge(v3, v5, 9);
-		addEdge(v5, v4, 7);
-		addEdge(v5, v1, 2);
-		addEdge(v4, v2, 2);
+	public static void addedge(vertex start,vertex end,int key){
+		edge e=new edge(start,end,key);
+		edgelist.add(e);	
 	}
-	public static void addEdge(Vertex a, Vertex b, int w) {
-		Edge e = new Edge(a, b, w);
-		Prim.EdgeQueue.add(e);
-	}
-	public static void primTree(){
-		buildGraph();
-		Vertex start = vertexList.get(0);
+	public static void prim(){
+		buildgraph();
+		vertex start=vertexlist.get(0);
 		newVertex.add(start);
-		for(int n=0;n<vertexList.size();n++){
-			Vertex temp = new Vertex(start.key);
-			Edge tempedge = new Edge(start,start,1000);
-			for(Vertex v : newVertex){
-				for(Edge e : EdgeQueue){
-					if(e.start==v && !containVertex(e.end)){
-						if(e.key<tempedge.key){
-							temp = e.end;
-							tempedge = e;
+		int minweight=0;
+		for(int i=0;i<vertexlist.size()-1;i++){
+			edge tmpedge=new edge(start,start,1000);
+			vertex temp=new vertex(start.key);
+			for(vertex v:newVertex){
+				for(edge e:edgelist){
+					if(e.start==v&&!containvertex(e.end)){
+						if(e.key<tmpedge.key){
+							temp=e.end;
+							tmpedge = e;
 						}
-							
-						
 					}
-				}
+					else if(e.end==v&&!containvertex(e.start)){
+						if(e.key<tmpedge.key){
+							temp=e.start;
+							tmpedge=e;
+						}
+					}
+				}		
 			}
+			System.out.println(tmpedge.start.key+" "+tmpedge.key+" "+tmpedge.end.key);
 			newVertex.add(temp);
+			minweight=minweight+tmpedge.key;
 		}
-		Iterator it = newVertex.iterator();
-		while(it.hasNext()){
-			Vertex v =(Vertex) it.next();
+		Iterator<vertex> iterator=newVertex.iterator();
+		while(iterator.hasNext()){
+			vertex v=iterator.next();
 			System.out.println(v.key);
 		}
+		System.out.println("最小权重为:"+minweight);
 	}
-	public static boolean containVertex(Vertex vte){
-		for(Vertex v : newVertex){
-			if(v.key.equals(vte.key))
+	public static boolean containvertex(vertex v){
+		for(vertex ver:newVertex){
+			if(ver.key.equals(v.key)){
 				return true;
+			}
 		}
 		return false;
 	}
-}
-class Vertex {
-	String key;
-	Vertex(String key){
-		this.key = key;
+	public static void main(String[] args) {
+		prim();
 	}
-}
 
-class Edge{
-	Vertex start;
-	Vertex end;
-	int key;
-	Edge(Vertex start,Vertex end,int key){
-		this.start = start;
-		this.end  = end;
-		this.key = key;
-		
-	}
 }
